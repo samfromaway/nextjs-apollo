29f157a00e07da59746e8864217276c5203b89c9
Sam <samfromaway@gmail.com>
2021-01-02 07:51:16 -0500

    change

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import { useQuery } from '@apollo/client';
 import GET_PRODUCT_BY_ID from '../lib/queries/getProductById';
 import { initializeApollo } from '../lib/apollo';
